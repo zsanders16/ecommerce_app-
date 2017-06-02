9c5c94cb628e77c9702d92acd0feee03f52f412f
Zachary Sanders <zsanders@icloud.com>
2017-06-02 16:35:27 -0600

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   
+  get 'shopping_carts/index'
+
+  get 'items/index'
+
+  get 'items/show'
+
+  get 'items/new'
+
+  get 'items/edit'
+
+  get 'categories/index'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
   devise_for :users
 
   
