f21013304620b82ee1882311a687050cbc0fae36
Zachary Sanders <zsanders@icloud.com>
2017-06-03 22:15:05 -0600

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ require 'pry'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  #@shopping_cart = ShoppingCart.find_by(user_id: current_user.id)
 end
