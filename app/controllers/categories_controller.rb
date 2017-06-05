fdf91dce891348e6c22cf39b847ce9a91c0bd93c
Josie Thompson <josiecthompson@gmail.com>
2017-06-05 11:58:49 -0600

    complete crud for category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 
 class CategoriesController < ApplicationController
-  before_action :set_categories, except: [:index, :new, :create]
+  before_action :set_category, except: [:index, :new, :create]
 #it will run before_action (set_categories) method for the show and edit.
   def index
     # Scope the categories down to the user that is logged in
@@ -29,20 +29,21 @@ class CategoriesController < ApplicationController
     # our before action is doing this for us
     # @category = Category.find(params[:id])
     if @category.update(category_params)
-      redirect_to @category
+      redirect_to categories_path
     else
       render :edit
     end
   end
 
   def destroy
+
     # our before action is doing this for us
     # @category = Category.find(params[:id])
     @category.destroy
-
+    
     # long hand way of setting flash messages
     flash[:notice] = 'Category Deleted!'
-    redirect_to category_path
+    redirect_to categories_path
   end
 
   private
