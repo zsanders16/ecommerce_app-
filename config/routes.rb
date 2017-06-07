0e7a52f3487a2f805e9c0e28a4a0c0c678475b06
Stockton <stocktonhedges@gmail.com>
2017-06-07 15:17:54 -0600

    env file

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
   resources :categories do
     resources :items
   end
-  
+
   resources :shopping_carts do
     resources :items do
       patch :add_item
       patch :remove_item
       end
   end
-  
 
-  
 
-  
+
+
+
   root "categories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
